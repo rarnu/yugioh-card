0d6caa3e5ca3ba7f9a4f59c62f6473d336aa1c90
rarnu <hexiaojie@hujiang.com>
2019-08-10 11:46:35 +0800

    update: to android-jvm 0.4.0

--- android/ygoapi/src/main/kotlin/com/rarnu/yugioh/YGORequest.kt
+++ android/ygoapi/src/main/kotlin/com/rarnu/yugioh/YGORequest.kt
@@ -1,15 +1,20 @@
 package com.rarnu.yugioh
 
-import com.rarnu.common.blockingHttp
+import android.util.Log
+import com.rarnu.common.http
 
 object YGORequest {
 
-    const val BASE_URL = "http://119.3.22.119"
+    // const val BASE_URL = "http://119.3.22.119"
+    const val BASE_URL = "http://122.112.175.64"
     const val RES_URL = "http://ocg.resource.m2v.cn/%d.jpg"
 
-    private fun request(aurl: String, param: String = "") = blockingHttp {
+    private fun request(aurl: String, param: String = "") = try { http {
         url = aurl
         getParam = param
+    } } catch (th: Throwable) {
+        Log.e("YGO", "error => $th")
+        null
     }
 
     fun search(akey: String, apage: Int) = request("$BASE_URL/search", "key=$akey&page=$apage")
