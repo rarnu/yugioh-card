b2c8ec7ec0476f7b040ce89ba9b85aaec9132398
rarnu <hexiaojie@hujiang.com>
2019-09-11 18:55:41 +0800

    update: flutter web

---
+++ flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rarnu/Develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rarnu/code/github/yugioh-card/flutter"
+export "FLUTTER_TARGET=/Users/rarnu/code/github/yugioh-card/flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rarnu/Develop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
