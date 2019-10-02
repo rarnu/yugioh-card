027ec72f833b56166dee92d6b773be7ffd81a195
rarnu <hexiaojie@hujiang.com>
2019-10-03 02:11:10 +0800

    add: image search for wxapp

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -6,10 +6,14 @@ Page({
     // private
     change: '<换一批>',
 
+    showCardPopup: false,
+    popupHeight: 0,
+
     keyword: '',
     hotwords: [],
     hotcards: [],
-    hotpacks: []
+    hotpacks: [],
+    imageids: [],
   },
   getHotestData: function() {
     var that = this
@@ -30,6 +34,103 @@ Page({
   },
   bindKeywordInput: function(e) {
       this.data.keyword = e.detail.value
+  },
+  findOneCard: function(imgid) {
+    var that = this
+    wx.request({
+      url: `${app.globalData.baseUrl}/findbyimage?imgid=${imgid}`,
+      success: res => {
+        if (res.data.result === 0) {
+          wx.navigateTo({
+            url: `../carddetail/carddetail?hash=${res.data.hash}`
+          })
+        } else {
+          wx.showToast({
+            title: '没有找到匹配的卡片.',
+            image: "../../images/error.png"
+          })
+        }
+      }
+    })
+  },
+  imageSearch: function(path) {
+    console.log(path)
+    var that = this
+    wx.uploadFile({
+      url: `${app.globalData.baseUrl}/matchimage`,
+      filePath: path,
+      name: 'file',
+      success(res) {
+        var json = JSON.parse(res.data)
+        if (json.result === 0) {
+          var imgids = json.imgids
+          if (imgids.length === 0) {
+            wx.showToast({
+              title: '没有找到匹配的卡片.',
+              image: "../../images/error.png"
+            })
+          } else if (imgids.length === 1) {
+            that.findOneCard(imgids[0])
+          } else {
+            var lines = parseInt(imgids.length / 5)
+            if (imgids.length % 5 !== 0) {
+              lines++
+            }            
+            that.setData({
+              popupHeight: lines * 163,
+              imageids: imgids,
+              showCardPopup: true
+            })
+          }
+        } else {
+          wx.showToast({
+            title: '上传图片失败.',
+            image: "../../images/error.png"
+          })
+        }
+      },
+    })
+  },
+
+  takePhoto: function() {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original'],
+      sourceType: ['camera'],
+      success(res) {
+        var tempFilePaths = res.tempFilePaths
+        that.imageSearch(tempFilePaths[0])
+      }
+    })
+  },
+  chooseLibrary: function() {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original'],
+      sourceType: ['album'],
+      success(res) {
+        var tempFilePaths = res.tempFilePaths
+        that.imageSearch(tempFilePaths[0])
+      }
+    })
+  },
+  bindImageTap: function(e) {
+    var that = this
+    wx.showActionSheet({
+      itemList: ['拍照', '从相册选取'],
+      success(res) {
+        switch(res.tapIndex) {
+          case 0:
+            that.takePhoto()
+            break
+          case 1:
+            that.chooseLibrary()
+            break
+        } 
+      },
+    })
   },
   btnSearchTap: function(e) {
     this.jumpToCardList()
@@ -94,5 +195,16 @@ Page({
   },
   bindChangeTap: function(e) {
     this.getHotestData()
+  },
+
+  popupCardTap: function(e) {
+    var imgid = e.currentTarget.dataset.id
+    this.findOneCard(imgid)
+  },
+
+  popupClose: function(e) {
+    this.setData({
+     showCardPopup: false
+    })
   }
 })
