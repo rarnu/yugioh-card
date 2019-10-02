07b951e5ab22d2af0319ea766a699bc35196991f
rarnu <hexiaojie@hujiang.com>
2019-10-02 16:26:37 +0800

    add: image search for android

--- android/app/src/main/java/com/rarnu/yugioh/card/MainActivity.kt
+++ android/app/src/main/java/com/rarnu/yugioh/card/MainActivity.kt
@@ -3,26 +3,33 @@ package com.rarnu.yugioh.card
 import android.Manifest
 import android.actionsheet.demo.com.khoiron.actionsheetiosforandroid.ActionSheet
 import android.actionsheet.demo.com.khoiron.actionsheetiosforandroid.Interface.ActionSheetCallBack
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ListView
+import android.support.design.internal.FlowLayout
+import android.view.*
+import android.widget.*
+import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.config.PictureConfig
+import com.luck.picture.lib.config.PictureMimeType
 import com.rarnu.android.*
 import com.rarnu.yugioh.HotCard
 import com.rarnu.yugioh.HotPack
 import com.rarnu.yugioh.YGOData
+import com.rarnu.yugioh.YGORequest
 import com.rarnu.yugioh.card.adapter.SimpleCardAdapter
 import com.rarnu.yugioh.card.adapter.SimplePackAdapter
 import com.rarnu.yugioh.card.adapter.SimpleSearchAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.net.URL
 import kotlin.concurrent.thread
+
 
 class MainActivity : Activity(), View.OnClickListener, AdapterView.OnItemClickListener {
 
@@ -38,7 +45,6 @@ class MainActivity : Activity(), View.OnClickListener, AdapterView.OnItemClickLi
     private lateinit var adapterPack: SimplePackAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         initUI()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -95,7 +101,7 @@ class MainActivity : Activity(), View.OnClickListener, AdapterView.OnItemClickLi
                         .setColorSelected(Color.DKGRAY)
                         .create(object : ActionSheetCallBack {
                             override fun data(data: String, position: Int) {
-                                when(position) {
+                                when (position) {
                                     0 -> takePhoto()
                                     1 -> choosePhotoLibrary()
                                 }
@@ -170,10 +176,147 @@ class MainActivity : Activity(), View.OnClickListener, AdapterView.OnItemClickLi
     }
 
     private fun takePhoto() {
-
+        PictureSelector.create(this)
+                .openCamera(PictureMimeType.ofImage())
+                .previewImage(false)
+                .forResult(PictureConfig.CHOOSE_REQUEST)
     }
 
     private fun choosePhotoLibrary() {
-
+        PictureSelector.create(this)
+                .openGallery(PictureMimeType.ofImage())
+                .selectionMode(PictureConfig.SINGLE)
+                .previewImage(false)
+                .isCamera(false)
+                .forResult(PictureConfig.CHOOSE_REQUEST)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                PictureConfig.CHOOSE_REQUEST -> {
+                    val path = PictureSelector.obtainMultipleResult(data).firstOrNull()?.path
+                    if (path != null && path != "") {
+                        toast(getString(R.string.toast_uploading))
+                        imageSearch(path)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun findOneCard(imgid: String) = thread {
+        val list = YGOData.findCardByImageId(imgid)
+        runOnMainThread {
+            if (list.isEmpty()) {
+                toast(getString(R.string.toast_no_matching_card))
+            } else {
+                startActivity(Intent(this, CardDetailActivity::class.java).apply {
+                    putExtra("hashid", list[0])
+                    putExtra("name", list[1])
+                })
+            }
+        }
+    }
+
+    private fun imageSearch(file: String) = thread {
+        val list = YGOData.imageSearch(File(file))
+        runOnMainThread {
+            when {
+                list.isEmpty() -> toast(getString(R.string.toast_no_matching_card))
+                list.size == 1 -> findOneCard(list[0])
+                else -> showPopupCard(list)
+            }
+        }
+    }
+
+    private fun showPopupCard(imgids: List<String>) {
+        val w = ((UI.width * 0.8 - 32.dip2px()) / 5).toInt()
+        val h = (w * 23 / 16)
+        val lines = imgids.size / 5 + (if (imgids.size % 5 != 0) 1 else 0)
+        val sw = (UI.width * 0.8).toInt()
+        val sh = lines * h + 96.dip2px()
+        val pop = PopupCardView(this).apply {
+            layoutParams = RelativeLayout.LayoutParams(sw, sh).apply {
+                addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE)
+            }
+            loadImages(imgids)
+        }
+        layRoot.addView(pop)
+    }
+
+    @SuppressLint("RestrictedApi")
+    inner class PopupCardView(context: Context?) : RelativeLayout(context) {
+
+        private val layCard: CardFlowLayout
+
+        init {
+            setBackgroundResource(R.drawable.view_round)
+            addView(TextView(context).apply {
+                text = "找到多张卡片"
+                setTextColor(Color.WHITE)
+                layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, 40.dip2px()).apply {
+                    topMargin = 4.dip2px()
+                    leftMargin = 8.dip2px()
+                }
+                textSize = 18f
+                gravity = Gravity.CENTER_VERTICAL
+            })
+
+            addView(Button(context).apply {
+                background = null
+                setTextColor(Color.WHITE)
+                text = "Close"
+                layoutParams = LayoutParams(65.dip2px(), 40.dip2px()).apply {
+                    addRule(ALIGN_PARENT_BOTTOM, TRUE)
+                    addRule(ALIGN_PARENT_RIGHT, TRUE)
+                    bottomMargin = 8.dip2px()
+                    rightMargin = 8.dip2px()
+                }
+                setOnClickListener {
+                    (this@PopupCardView.parent as RelativeLayout).removeView(this@PopupCardView)
+                }
+            })
+
+            layCard = CardFlowLayout(context).apply {
+                layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
+                    topMargin = 48.dip2px()
+                    leftMargin = 8.dip2px()
+                }
+            }
+            addView(layCard)
+        }
+
+        fun loadImages(imgids: List<String>) {
+            val w = ((UI.width * 0.8 - 32.dip2px()) / 5).toInt()
+            val h = 23 * w / 16
+            imgids.forEach { p ->
+                layCard.addView(ImageView(context).apply {
+                    layoutParams = ViewGroup.LayoutParams(w, h)
+                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    isClickable = true
+                    isFocusable = true
+                    thread {
+                        val img = BitmapFactory.decodeStream(URL(YGORequest.RES_URL.format(p.toInt())).openStream())
+                        runOnMainThread {
+                            setImageBitmap(img)
+                        }
+                    }
+                    setOnClickListener {
+                        findOneCard(p)
+                    }
+                })
+            }
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    inner class CardFlowLayout(context: Context?) : FlowLayout(context) {
+        init {
+            itemSpacing = 4.dip2px()
+            lineSpacing = 4.dip2px()
+        }
+    }
+
 }
+
